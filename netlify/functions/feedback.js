168b7886a766b157653e4b3d0ff799a21dba4102
Vitorkm <vhkm12@gmail.com>
2025-08-19 13:52:09 -0300

    FIX: Corrige mensagem de erro

--- netlify/functions/feedback.js
+++ netlify/functions/feedback.js
@@ -67,6 +67,13 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ message: 'Feedback enviado com sucesso', data: feedback }),
     };
   } catch (error) {
+    if(empresa > 50){
+      return {
+        statusCode: 400,
+        headers,
+        body: JSON.stringify({ error: 'Nome da empresa não pode exceder 50 caracteres' }),
+      };
+    }
     console.error('Erro ao salvar feedback:', error);
     return {
       statusCode: 500,
